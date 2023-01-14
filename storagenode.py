a9f3a2004794520914c3072dc3d4966ec5425a75
aanrv <varan9595@yahoo.com>
2023-01-14 16:59:45 -0500

    read/write in binary mode, fix inefficient repeated encodes/decodes

--- storagenode.py
+++ storagenode.py
@@ -22,7 +22,8 @@ class StorageNode(Node):
         self._dataDir = os.path.expandvars(dataDir)
         os.makedirs(self._dataDir, exist_ok=True)
 
-    def sendDataAdd(self, host, port, filename=''):
+    def sendDataAdd(self, host, port, filename):
+        filename = os.path.expandvars(filename)
         self._logger.info('sending data add to %s:%s' % (host, port))
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         clientSocket.connect((host, port))
@@ -30,11 +31,11 @@ class StorageNode(Node):
         buffer = StorageNode.DELIM.join(map(str, (RequestType.DATA_ADD.value, dataSize))) + StorageNode.DELIM
         bytesRemaining = dataSize
         clientSocket.send(buffer.encode())
-        with open(filename, 'r') as f:
+        with open(filename, 'rb') as f:
             while bytesRemaining:
                 assert(bytesRemaining > 0)
                 data = f.read(4096)
-                clientSocket.send(data.encode())
+                clientSocket.send(data)
                 bytesRemaining -= len(data)
         clientSocket.close()
 
@@ -49,17 +50,18 @@ class StorageNode(Node):
         clientSocket.send(buffer.encode())
         self._logger.info('receiving')
         # keep recv'ing until data size can be parsed
-        recvBuffer = clientSocket.recv(4096).decode()
-        while (recvBuffer.count(StorageNode.DELIM) < 1):
-            recvBuffer += clientSocket.recv(4096).decode()
-        dataSize = int(recvBuffer.split(Node.DELIM)[0])
-        data = recvBuffer.split(StorageNode.DELIM)[1]
-        tmp = tempfile.NamedTemporaryFile(mode='w+', delete=False)
+        DELIM_ENCODED = Node.DELIM.encode()
+        recvBuffer = clientSocket.recv(4096)
+        while (recvBuffer.count(DELIM_ENCODED) <= 0):
+            recvBuffer += clientSocket.recv(4096)
+        dataSize = int(recvBuffer.split(DELIM_ENCODED)[0].decode())
+        data = int(recvBuffer.split(DELIM_ENCODED)[1])
+        tmp = tempfile.NamedTemporaryFile(mode='w+b', delete=False)
         totalBytesWritten = 0
         totalBytesWritten += tmp.write(data)
         # keep reading until dataSize bytes are read
         while (totalBytesWritten < dataSize):
-            data = clientSocket.recv(4096).decode()
+            data = clientSocket.recv(4096)
             totalBytesWritten += tmp.write(data)
         assert(totalBytesWritten == dataSize)
         os.rename(tmp.name, targetfile)
@@ -75,29 +77,31 @@ class StorageNode(Node):
         clientSocket.close()
 
     def _handleDataAdd(self, buffer, connection):
-        buffer = buffer.decode()
-        # TODO handle binary files
-        while (buffer.count(Node.DELIM) <= Fields[RequestType.DATA_ADD].SIZE.value):
-            buffer += connection.recv(4096).decode()
-        dataSize = int(buffer.split(StorageNode.DELIM)[Fields[RequestType.DATA_ADD].SIZE.value])
+        # keep reading until able to parse data size
+        # will almost always only run once
+        DELIM_ENCODED = Node.DELIM.encode()
+        while (buffer.count(DELIM_ENCODED) <= Fields[RequestType.DATA_ADD].SIZE.value):
+            buffer += connection.recv(4096)
+        dataSize = int(buffer.split(DELIM_ENCODED)[Fields[RequestType.DATA_ADD].SIZE.value].decode())
         # get data currently in buffer
-        data = buffer.split(StorageNode.DELIM)[Fields[RequestType.DATA_ADD].DATA.value]
+        data = buffer.split(DELIM_ENCODED)[Fields[RequestType.DATA_ADD].DATA.value]
         # write to temporary file
-        tmp = tempfile.NamedTemporaryFile(mode='w+', delete=False)
+        tmp = tempfile.NamedTemporaryFile(mode='w+b', delete=False)
         totalBytesWritten = 0
         totalBytesWritten += tmp.write(data)
         # datahash will be output filename
         datahash = hashlib.sha256()
-        datahash.update(data.encode())
+        datahash.update(data)
         # while dataSize bytes not written, keep recv'ing and writing
         while (totalBytesWritten < dataSize):
-            data = connection.recv(4096).decode()
+            data = connection.recv(4096)
             totalBytesWritten += tmp.write(data)
-            datahash.update(data.encode())
-        assert(totalBytesWritten == dataSize)
+            datahash.update(data)
+        print('WRITTEN %s SIZE %s' % (totalBytesWritten, dataSize))
         outfilename = os.path.join(self._dataDir, datahash.hexdigest())
         os.rename(tmp.name, outfilename)
         tmp.close()
+        assert(totalBytesWritten == dataSize)
 
     def _handleDataGet(self, buffer, connection):
         buffer = buffer.decode()
@@ -108,10 +112,10 @@ class StorageNode(Node):
         bytesRemaining = os.path.getsize(fullfile)
         outbuffer = str(bytesRemaining) + StorageNode.DELIM
         connection.send(outbuffer.encode())
-        with open(fullfile, 'r') as f:
+        with open(fullfile, 'rb') as f:
             while bytesRemaining:
                 data = f.read(4096)
-                connection.send(data.encode())
+                connection.send(data)
                 bytesRemaining -= len(data)
                 assert(bytesRemaining >= 0)
             assert(bytesRemaining == 0)
