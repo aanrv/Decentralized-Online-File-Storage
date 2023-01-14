37059a9b90e6ac901e3cbb4c6299b3a28251811e
aanrv <varan9595@yahoo.com>
2023-01-14 00:11:26 -0500

    add size field to send data, add request field enums to common

--- common.py
+++ common.py
@@ -2,13 +2,40 @@
 
 from enum import Enum
 
+# possible request message types sent by local host to a remote host
 RequestType = Enum('RequestType', [
-    'PING',
-    'CONNECT',
-    'DISCONNECT',
-    'GET_PEERS',
-    'DATA_ADD',
-    'DATA_GET',
-    'DATA_REMOVE',
+    'PING',         # a socket connect and disconnect
+    'CONNECT',      # request to connect i.e. add one another to peers lists
+    'DISCONNECT',   # request to remove one another from peers list
+    'GET_PEERS',    # request remote host's peers list
+    'DATA_ADD',     # request remote host to add provided data to its storage directory
+    'DATA_GET',     # request data with the provided hash
+    'DATA_REMOVE',  # request remote host to remove data with the provided hash from its storage directory
+])
+
+# request message field indices when split by delim
+# starts with 1 because 0 is always the message type
+
+ConnectRequestFields = Enum('ConnectFields', [
+    'HOST',
+    'PORT',
+])
+
+DisconnectRequestFields = Enum('DisconnectFields', [
+    'HOST',
+    'PORT',
+])
+
+DataAddRequestFields = Enum('DataAddFields', [
+    'SIZE',
+    'DATA',
+])
+
+DataGetRequestFields = Enum('DataGetFields', [
+    'HASH',
+])
+
+DataRemoveRequestFields = Enum('DataRemoveFields', [
+    'HASH',
 ])
 
--- storagenode.py
+++ storagenode.py
@@ -1,6 +1,6 @@
 # storagenode.py
 
-from common import RequestType
+from common import *    # RequestType and *RequestFields types
 from node import Node
 import os
 import socket
@@ -26,7 +26,7 @@ class StorageNode(Node):
     def sendDataAdd(self, host, port, data):
         # TODO remove and handle large files
         assert(len(data) < 4000)
-        buffer = StorageNode.DELIM.join(map(str, (RequestType.DATA_ADD.value, data))) + StorageNode.DELIM
+        buffer = StorageNode.DELIM.join(map(str, (RequestType.DATA_ADD.value, str(len(data)), data))) + StorageNode.DELIM
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         clientSocket.connect((host, port))
         clientSocket.send(buffer.encode())
@@ -40,8 +40,8 @@ class StorageNode(Node):
         self._logger.info('receiving')
         data = clientSocket.recv(4096).decode()
         data = data.split(Node.DELIM)[0]
-        print('GOT %s' % data)
         clientSocket.close()
+        return data
 
     def sendDataRemove(self, host, port, datahash):
         buffer = StorageNode.DELIM.join(map(str, (RequestType.DATA_REMOVE.value, datahash))) + StorageNode.DELIM
@@ -51,19 +51,19 @@ class StorageNode(Node):
         clientSocket.close()
 
     def _handleDataAdd(self, buffer, _):
-        data = buffer.split(StorageNode.DELIM)[1]
+        _, dataSize, data = buffer.split(StorageNode.DELIM)[:-1]
         filename = hashlib.sha256(data.encode()).hexdigest()
         with open(os.path.join(self._dataDir, filename), 'w') as f:
             f.write(data)
 
     def _handleDataGet(self, buffer, connection):
-        filename = buffer.split(StorageNode.DELIM)[1]
+        filename = buffer.split(StorageNode.DELIM)[DataGetRequestFields.HASH.value]
         with open(os.path.join(self._dataDir, filename), 'r') as f:
             data = f.read()
         buffer = data + StorageNode.DELIM
         connection.send(buffer.encode())
 
     def _handleDataRemove(self, buffer, _):
-        filename = buffer.split(StorageNode.DELIM)[1]
+        filename = buffer.split(StorageNode.DELIM)[DataRemoveRequestFields.HASH.value]
         os.remove(os.path.join(self._dataDir, filename))
 
